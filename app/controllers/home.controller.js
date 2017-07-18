9e4018c1f57dfa00f4b0529119e8e8a99ae7ed92
Ross Ewing <mail@smc.digital>
2017-07-18 22:48:35 +0100

    Added google maps and style
    not responsive and need to export maps script into a service

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -9,11 +9,13 @@ app.controller('homeWeather', function($scope) {
     var temp = weather.main.temp_max - 270.15;
     $scope.temp = "Temperature: " + temp + "°C";
     console.log(weather);
+    var lat = weather.coord.lat;
+    var long = weather.coord.lon;
+    getMap(long, lat);
     i++
   };
 
   var swears = ['fucking', 'damn', 'crappy', 'shitty', 'tossing', 'fucking'];
-
 
   function getWeather(town) {
     var token = '&APPID=def3afd6110da830af14acce613b0091';
@@ -29,4 +31,160 @@ app.controller('homeWeather', function($scope) {
     }).responseText);
     $scope.loading = true;
   };
+
+  function getMap(long, lat) {
+    showGoogleMaps();
+
+    function showGoogleMaps() {
+      var latLng = new google.maps.LatLng(lat, long);
+
+      var mapOptions = {
+        zoom: 16, // initialize zoom level - the max value is 21
+        streetViewControl: false, // hide the yellow Street View pegman
+        scaleControl: false, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: latLng,
+        styles: [{
+            elementType: 'geometry',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            elementType: 'labels.text.stroke',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#FFFFFF'
+            }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          },
+          {
+            elementType: "labels",
+            stylers: [{
+              visibility: 'off'
+            }]
+          },
+          {
+            disableDefaultUI: true
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{
+              color: '#f2f2f2'
+            }]
+          }
+        ]
+      };
+
+      map = new google.maps.Map(document.getElementById('googlemaps'),
+        mapOptions);
+      // Show the default red marker at the location
+    }
+
+    google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+  };
+
 });
